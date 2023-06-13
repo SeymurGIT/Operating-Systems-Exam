47a8ee591426a33fbad6ea976cae2971c6136436
SeymurGIT <seymur.2004.78@mail.ru>
2023-06-14 00:54:59 +0400

    Deleted

--- Fibonacci.js
+++
@@ -1,9 +0,0 @@
-var terms = 10;
-var fibSeries = [0, 1];
-for (var i = 2; i < terms; i++) {
-  fibSeries[i] = fibSeries[i - 1] + fibSeries[i - 2];
-}
-WScript.Echo("Fibonacci series:");
-for (var i = 0; i < terms; i++) {
-  WScript.Echo(fibSeries[i]);
-}
